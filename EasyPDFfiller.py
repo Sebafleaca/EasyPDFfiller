982e7caa1f8af29260afc20a05060c4c6954939e
Sebafleaca <sebastian.fleaca@hotmail.com>
2023-04-09 23:53:47 +0200

    find ways to retrieve datafrom PDF

--- EasyPDFfiller.py
+++ EasyPDFfiller.py
@@ -8,17 +8,35 @@ num_pages = read_PDF._get_num_pages()
 
 # Split all the text in pages
 text = []
+xform_text = []
 index=0
 for page in pages:
-    for x in page.get_contents():
-        print(x)
+    
+    ''' What can be retrieved from a PDF page?'''
+
+    # This only prints "/Filter"
+    for content in page.get_contents():
+        print(content)
+    # This prints "{'/Filter: '/FlateDecode'}"
+    print(page.get_contents())
+    # This prints contents in XML format
+    print(page.get_contents)
+    # This prints all text in XML format
     print(page.extract_xform_text)
-    text.insert(index, page.extract_text())
+    # This prints all text in plain text
+    print(page.extract_text)
+
+    # Doesn't work
+    # for content in page.get_contents():
+    #    print(page.extract_xform_text(content))
+
+    xform_text.insert(index, page.extract_xform_text)
+    text.insert(index, page.extract_text)
     index=index+1
 
 # Retrieve text of all pages
 index=0
 for page in pages:
-    print("Retrieving page number " + str(index) + ": ")
+    print("\nRetrieving page number " + str(index) + ": ")
     print(text[index])
     index=index+1
